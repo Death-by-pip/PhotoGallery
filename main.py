51bc54a985f9e9615d6b67c1fbdc1474d5ebf8c3
Death-by-pip <innerprotext@gmail.com>
2023-10-23 13:25:20 -0500

    completed functions, particularly pixelation. (black and white filter not yet added)

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.dropdown import DropDown
 from PIL import Image, ImageDraw
 import random
+
+
+def
 
 
 def invert(image):
@@ -109,10 +113,13 @@ def rainbow_image(image):
 
 def pixelate(image, X, Y, width, height, intensity: float):
     pixels = image.load()
-    intensityF = 0.5 - ((2 * (intensity - 1) ** 3) / 2)
+    intensityF = .5 - ((2*intensity - 1) ** 3)/2
     sub_area = (height * width) ** intensityF
     swidth = (sub_area * width / height) ** (1 / 2)
     sheight = (sub_area * height / width) ** (1 / 2)
+    print(X, Y, width, height)
+    print(image.width, image.height)
+    print("----")
     for x in range(round(swidth)):
         for y in range(round(sheight)):
             r = []
@@ -122,6 +129,7 @@ def pixelate(image, X, Y, width, height, intensity: float):
             X2 = X + round(width * (x + 1) / round(swidth))
             Y1 = Y + round(height * y / round(sheight))
             Y2 = Y + round(height * (y + 1) / round(sheight))
+            print(X1, Y1, X2, Y2)
             # print(X1," ",X2," ",Y1," ",Y2)
             for x1 in range(X1, X2):
                 for y1 in range(Y1, Y2):
@@ -171,23 +179,99 @@ class Display(Screen):
     def __init__(self, **kwargs):
         super(Display, self).__init__(**kwargs)
         self.filter = None
+        self.image = Image.new("RGB", (512, 512), 0)
+        self.image.save("TEMP.jpg")
+        self.coords = [(0, 0), (1, 1)]
         # self.ids.selector.bind(on_update=lambda instance, x: self.on_selection(x))
 
     def update(self):
         # global index
         # index = index%len(images)
-        self.ids.image.source = self.ids.filename.text
+        self.image = Image.open(self.ids.filename.text)
+        self.image.save("TEMP.jpg")
+        self.ids.image.reload()
 
     def on_selection(self, data):
+        # if self.filter == "pixelate":
+        #     self.ids.normalapply.text = "Apply Filter"
+        #     self.ids.toggleapply.text = ""
+        #     self.ids.normalapply.size_hint = (1,1)
+        #     self.ids.toggleapply.size_hint = (0,0)
         self.filter = data
+        self.applypixel = False
+        if self.filter in ["pixelate", "line_drawing"]:
+            self.ids.intensity.disabled = False
+        else:
+            self.ids.intensity.disabled = True
+        # if self.filter == "pixelate":
+        #     self.ids.normalapply.text = ""
+        #     self.ids.toggleapply.text = "Apply Filter"
+        #     self.ids.normalapply.size_hint = (0,0)
+        #     self.ids.toggleapply.size_hint = (1,1)
 
     def apply(self):
-        img = self.ids.image.source
-        if self.filter != None:
-            img = Image.open(img)
-            img = eval(self.filter+"(img)")
-            img.save("TEMP.jpg")
-            self.ids.image.source = "TEMP.jpg"
+        if self.filter != "pixelate":
+            self.applypixel = False
+            self.ids.applier.state = "normal"
+            if self.filter != "line_drawing":
+                self.image = eval(self.filter + "(self.image)", globals(), locals())
+            elif self.filter == "line_drawing":
+                self.image = line_drawing(self.image, float(self.ids.intensity.text))
+            self.image.save("TEMP.jpg")
+            self.ids.image.reload()
+
+    def on_touch_down(self, touch):
+        x, y = touch.x, touch.y
+        self.coords = [(x, y)]
+        touch.push()
+        touch.apply_transform_2d(self.to_local)
+        ret = super(RelativeLayout, self).on_touch_down(touch)
+        touch.pop()
+        return ret
+
+    # def on_touch_move(self, touch):
+    #     x, y = touch.x, touch.y
+    #     touch.push()
+    #     touch.apply_transform_2d(self.to_local)
+    #     ret = super(RelativeLayout, self).on_touch_move(touch)
+    #     touch.pop()
+    #     return ret
+
+    def on_touch_up(self, touch):
+        x, y = touch.x, touch.y
+        self.coords.append((x, y))
+        if self.ids.applier.state == "down" and not self.applypixel:
+            self.applypixel = True
+        elif self.filter == "pixelate" and self.applypixel:
+            pos = (self.ids.image.center_x - self.ids.image.norm_image_size[0] / 2, self.ids.image.center_y - self.ids.image.norm_image_size[1] / 2)
+            W = self.ids.image.norm_image_size[0]
+            H = self.ids.image.norm_image_size[1]
+            print(pos, W, H)
+            C = [i for i in self.coords[1]]
+            c = [i for i in self.coords[0]]
+            print(C, c)
+            if pos[0]<C[0]<pos[0]+W and pos[0]<c[0]<pos[0]+W and pos[1]<C[1]<pos[1]+H and pos[1]<c[1]<pos[1]+H:
+                C[0] -= pos[0]
+                c[0] -= pos[0]
+                C[1] = pos[1] + H - C[1]
+                c[1] = pos[1] + H - c[1]
+                print(C, c)
+                width = abs(C[0] - c[0])
+                height = abs(C[1] - c[1])
+                X = min(C[0], c[0])
+                Y = min(C[1], c[1])
+                print(X, Y, width, height)
+                self.image = pixelate(self.image, int(X), int(Y), int(width), int(height), float(self.ids.intensity.text))
+                self.image.save("TEMP.jpg")
+                self.ids.image.reload()
+                self.ids.applier.state = "normal"
+                self.applypixel = False
+
+        touch.push()
+        touch.apply_transform_2d(self.to_local)
+        ret = super(RelativeLayout, self).on_touch_up(touch)
+        touch.pop()
+        return ret
     # def display_image(self):
     #     return images[index]
 
